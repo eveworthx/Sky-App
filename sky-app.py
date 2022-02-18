87a36b090819548779a76b650b12b09c649c0adf
eveworthx <eveworthx@gmail.com>
2022-02-18 23:34:39 +0000

    added form and html templates

--- sky-app.py
+++ sky-app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello there, guys!</p>"
+def hello():
+    return "<p>Hello! Welcome to my web app</p>"
+
+@app.route("/form/")
+def form():
+    return render_template('form.html')
+
+@app.route('/success', methods = ['POST', 'GET'])
+def success():
+   if request.method == 'POST':
+      success = request.form
+      return render_template("success.html", success = success)
